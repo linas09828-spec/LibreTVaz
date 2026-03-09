35d9716f35f3e46d15f2e7ae9e59873aa38ab16e
linas09828-spec <linas09828@gmail.com>
2026-03-09 16:28:39 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,22 +1,22 @@
 const CUSTOMER_SITES = {
     haohua: {
-        api: 'https://hhzyapi.com/api.php/provide/vod/',
+        api: 'https://hhzyapi.com/api.php/provide/vod',
         name: '豪华资源',
     },
     hongniu: {
-        api: 'https://www.hongniuzy2.com/api.php/provide/vod/',
+        api: 'https://www.hongniuzy2.com/api.php/provide/vod',
         name: '红牛资源',
     },
     okzy: {
-        api: 'https://api.okzyw.net/api.php/provide/vod/',
+        api: 'https://api.okzyw.net/api.php/provide/vod',
         name: 'OK资源',
     },
     zuida: {
-        api: 'https://api.zuidapi.com/api.php/provide/vod/',
+        api: 'https://api.zuidapi.com/api.php/provide/vod',
         name: '最大资源',
     },
     liangzi: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod/',
+        api: 'https://cj.lziapi.com/api.php/provide/vod',
         name: '量子资源',
     }
 };
